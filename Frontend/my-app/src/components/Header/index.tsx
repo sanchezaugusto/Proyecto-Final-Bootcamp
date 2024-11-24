941432575efb972e924320ce6875605175814c78
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-11-24 20:20:45 -0300

    Add user session

--- Frontend/my-app/src/components/Header/index.tsx
+++ Frontend/my-app/src/components/Header/index.tsx
@@ -1,8 +1,13 @@
 import Link from 'next/link';
 import React from 'react';
 import './Header.css'
+import { auth, signOut } from '@/auth';
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth()
+
+  const isLogged = session?.user
+  console.log(isLogged)
   return (
     <header className='header-nav flex py-2 justify-center w-full sticky top-0 z-10'>
       <div className="container mx-auto grid grid-cols-6 items-center px-6 py-4">
@@ -27,18 +32,35 @@ export default function Header() {
         </nav>
 
         {/* Auth Buttons */}
-        <div className="flex justify-end gap-4 col-span-1">
-          <Link className="px-4 py-2 text-sm font-medium text-gray-900 bg-white rounded-md shadow hover:bg-gray-100 transition-all" href="/login">
+        {!isLogged ? (
+          <div className="flex justify-end gap-4 col-span-1">
+            <Link className="px-4 py-2 text-sm font-medium text-gray-900 bg-white rounded-md shadow hover:bg-gray-100 transition-all" href="/login">
 
-            Iniciar Sesión
+              Iniciar Sesión
 
-          </Link>
-          <Link className="px-4 py-2 text-sm font-medium text-white bg-gray-900 rounded-md shadow hover:bg-gray-600 transition-all" href="/register">
+            </Link>
+            <Link className="px-4 py-2 text-sm font-medium text-white bg-gray-900 rounded-md shadow hover:bg-gray-600 transition-all" href="/register">
 
-            Registrarse
+              Registrarse
 
-          </Link>
-        </div>
+            </Link>
+          </div>
+        ): (
+          <div className="flex justify-end gap-6 col-span-1">
+            <Link href={"/cart"} className='w-[30px] h-[30px]'>
+              <img className='' src="/cart.png" alt="Cart" />
+            </Link>
+            <form
+              action={async () => {
+                "use server"
+                await signOut()
+              }}
+            >
+              <button className="px-4 py-2 text-sm font-medium text-white bg-gray-900 rounded-md shadow hover:bg-gray-600 transition-all" type="submit">Sign Out</button>
+            </form>
+          </div>
+        )}
+        
       </div>
     </header>
   );
