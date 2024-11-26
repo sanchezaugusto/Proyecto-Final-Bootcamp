6c19287d3ce2de34f72fa72944b814b18de0a9d3
LuqueIgnacio <ignacioluque.micha@gmail.com>
2024-11-26 17:37:00 -0300

    Add filters to search products

--- Frontend/my-app/src/services/categoryService.ts
+++ Frontend/my-app/src/services/categoryService.ts
@@ -2,7 +2,6 @@ export async function getCategories(){
     try{
        const query = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/categories`)
        const categories = await query.json()
-       console.log(`${process.env.NEXT_PUBLIC_API_HOST}/categories/`, categories) 
        return categories
     }catch (error) {
         console.log(error)
