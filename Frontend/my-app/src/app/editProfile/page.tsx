7c5d7c9881e39dfe251eb07e651e44f021d29550
Goytia Jeremias <jeremiasjg99@gmail.com>
2024-11-28 20:46:17 -0300

    edit profile page redesign

--- Frontend/my-app/src/app/editProfile/page.tsx
+++ Frontend/my-app/src/app/editProfile/page.tsx
@@ -100,108 +100,134 @@ const UserProfile: React.FC = () => {
   }
 
   return (
-    <div className="max-w-2xl mx-auto p-6 bg-white rounded-lg shadow-lg">
-      <div className="flex items-center mb-6">
-        <img src={formData.image || 'https://via.placeholder.com/150'} alt="User" className="w-24 h-24 rounded-full mr-4" />
-        {editMode ? (
-          <div className="flex flex-col">
+    <div className='bg-gray-100 min-h-[80vh] flex flex-col items-center justify-center'>
+
+
+      <div className="max-w-4xl mx-auto p-12 bg-white rounded-2xl shadow-lg grid justify-items-center items-center justify-center grid-cols-3 gap-2">
+        {/* Primera columna: Imagen de perfil */}
+        <div className="flex flex-col items-center">
+          <img
+            src={formData.image || 'https://via.placeholder.com/150'}
+            alt="User"
+            className="size-64 rounded-full border-4 border-gray-200 shadow-md mb-4"
+          />
+          {editMode ? (
             <input
               type="file"
               accept="image/*"
               onChange={handleFileChange}
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              className="mt-2 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             />
-          </div>
-        ) : (
-          <button
-            onClick={() => setEditMode(true)}
-            className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Editar Imagen
-          </button>
-        )}
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Nombre</label>
-        {editMode ? (
-          <input
-            type="text"
-            name="first_name"
-            value={formData.first_name}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        ) : (
-          <p className="text-gray-800">{user.first_name}</p>
-        )}
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Apellido</label>
-        {editMode ? (
-          <input
-            type="text"
-            name="last_name"
-            value={formData.last_name}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        ) : (
-          <p className="text-gray-800">{user.last_name}</p>
-        )}
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Nombre de Usuario</label>
-        {editMode ? (
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        ) : (
-          <p className="text-gray-800">{user.username}</p>
-        )}
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Correo Electrónico</label>
-        {editMode ? (
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        ) : (
-          <p className="text-gray-800">{user.email}</p>
-        )}
-      </div>
-      {editMode && (
-        <div className="flex space-x-4">
-          <button
-            onClick={handleSave}
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-          >
-            Guardar Cambios
-          </button>
-          <button
-            onClick={handleCancel}
-            className="mt-4 px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600"
-          >
-            Cancelar
-          </button>
+          ) : (
+            <button
+              onClick={() => setEditMode(true)}
+              className="px-4 py-2 text-white rounded-md shadow-xl hover:scale-110 transition-all"
+            >
+              <svg xmlns="http://www.w3.org/2000/svg" width={32} height={32} enableBackground="new 0 0 32 32" viewBox="0 0 32 32" id="edit">
+                <path d="M12.82373,12.95898l-1.86279,6.21191c-0.1582,0.52832-0.01367,1.10156,0.37646,1.49121c0.28516,0.28516,0.66846,0.43945,1.06055,0.43945c0.14404,0,0.28906-0.02051,0.43066-0.06348l6.2124-1.8623c0.23779-0.07129,0.45459-0.2002,0.62988-0.37598L31.06055,7.41016C31.3418,7.12891,31.5,6.74707,31.5,6.34961s-0.1582-0.7793-0.43945-1.06055l-4.3501-4.34961c-0.58594-0.58594-1.53516-0.58594-2.12109,0L13.2002,12.3291C13.02441,12.50488,12.89551,12.7207,12.82373,12.95898z M15.58887,14.18262L25.6499,4.12109l2.22852,2.22852L17.81738,16.41113l-3.18262,0.9541L15.58887,14.18262z"></path>
+                <path d="M30,14.5c-0.82861,0-1.5,0.67188-1.5,1.5v10c0,1.37891-1.12158,2.5-2.5,2.5H6c-1.37842,0-2.5-1.12109-2.5-2.5V6c0-1.37891,1.12158-2.5,2.5-2.5h10c0.82861,0,1.5-0.67188,1.5-1.5S16.82861,0.5,16,0.5H6C2.96729,0.5,0.5,2.96777,0.5,6v20c0,3.03223,2.46729,5.5,5.5,5.5h20c3.03271,0,5.5-2.46777,5.5-5.5V16C31.5,15.17188,30.82861,14.5,30,14.5z"></path>
+              </svg>
+            </button>
+          )}
         </div>
-      )}
-      {!editMode && (
-        <button
-          onClick={() => setEditMode(true)}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
-        >
-          Editar Perfil
-        </button>
-      )}
+
+        {/* Segunda columna: Nombre y Apellido */}
+        <div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-xl font-semibold">Nombre</label>
+            {editMode ? (
+              <input
+                type="text"
+                name="first_name"
+                value={formData.first_name}
+                onChange={handleChange}
+                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            ) : (
+              <p className="text-gray-500 font-medium">{user.first_name}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-xl font-semibold">Apellido</label>
+            {editMode ? (
+              <input
+                type="text"
+                name="last_name"
+                value={formData.last_name}
+                onChange={handleChange}
+                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            ) : (
+              <p className="text-gray-500 font-medium">{user.last_name}</p>
+            )}
+          </div>
+        </div>
+
+        {/* Tercera columna: Nombre de Usuario y Correo Electrónico */}
+        <div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-xl font-semibold">Nombre de Usuario</label>
+            {editMode ? (
+              <input
+                type="text"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            ) : (
+              <p className="text-gray-500 font-medium">{user.username}</p>
+            )}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-xl font-semibold">Correo Electrónico</label>
+            {editMode ? (
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            ) : (
+              <p className="text-gray-500 font-medium">{user.email}</p>
+            )}
+          </div>
+        </div>
+
+        {/* Segunda fila: Botones */}
+        <div className="col-span-full w-full flex justify-end space-x-4 mt-6">
+          {editMode ? (
+            <>
+              <button
+                onClick={handleSave}
+                className="px-4 py-2 bg-[#2a2a2a] text-white rounded-md hover:bg-gray-800 shadow-md"
+              >
+                Guardar Cambios
+              </button>
+              <button
+                onClick={handleCancel}
+                className="px-4 py-2 bg-gray-500 text-white rounded-md hover:bg-gray-600 shadow-md"
+              >
+                Cancelar
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => setEditMode(true)}
+              className="px-4 py-2 bg-[#2a2a2a] text-white rounded-md hover:bg-gray-800 shadow-md"
+            >
+              Editar Perfil
+            </button>
+          )}
+        </div>
+
+
+      </div>
+
+
     </div>
+
   );
 };
 
