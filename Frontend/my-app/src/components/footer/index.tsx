056b0fac221a53f83ae5b47852823e5960e5c513
Rodrigo Benitez <roddrigo.benitez@gmail.com>
2024-11-29 15:18:52 -0300

    Resulto para el deploy con vercel

--- Frontend/my-app/src/components/footer/index.tsx
+++ Frontend/my-app/src/components/footer/index.tsx
@@ -91,7 +91,7 @@ export default function Footer() {
               <textarea
                 id="message"
                 placeholder="Tu mensaje"
-                rows="4"
+                rows= {4}
                 className="w-full bg-gray-100 text-white rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-gray-200"
               ></textarea>
             </div>
