d5d2e73f18dcf5ffc6be5ba6b0c7986c80d7821e
sanchezaugusto <sanchez.augusto.n@gmail.com>
2024-11-28 21:42:09 -0300

    prueba deploy backend

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,3 +1,31 @@
+// import express from "express";
+// import dbConnect from "./db/dbConnect";
+// import dotenv from "dotenv";
+// import router from "./routes/index";
+// import cors from "cors";
+
+// dotenv.config();
+
+// const PORT = parseInt(process.env.PORT ?? "5000", 10);
+// const HOST = process.env.HOST ?? "localhost";
+
+// const app = express();
+
+// app.use(cors({
+//   origin: "http://localhost:3000", // Ajusta esto al origen de tu frontend
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// }));
+
+// app.use(express.json());
+
+// app.use("/api", router);
+
+// dbConnect();
+
+// app.listen(PORT, HOST, () => {
+//   console.log(`Server is running on http://${HOST}:${PORT}`);
+// });
+
 import express from "express";
 import dbConnect from "./db/dbConnect";
 import dotenv from "dotenv";
@@ -6,13 +34,12 @@ import cors from "cors";
 
 dotenv.config();
 
-const PORT = parseInt(process.env.PORT ?? "5000", 10);
-const HOST = process.env.HOST ?? "localhost";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:3000", // Ajusta esto al origen de tu frontend
+  origin: process.env.CORS_ORIGIN || "http://localhost:3000", // Ajusta esto al origen de tu frontend
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
@@ -22,6 +49,6 @@ app.use("/api", router);
 
 dbConnect();
 
-app.listen(PORT, HOST, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
