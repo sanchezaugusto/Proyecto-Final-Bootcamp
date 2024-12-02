3d143e056e77344ccc463120ac20df47040d8cd0
sanchezaugusto <sanchez.augusto.n@gmail.com>
2024-12-01 23:36:33 -0300

    Backend: clean code

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,31 +1,3 @@
-// import express from "express";
-// import dbConnect from "./db/dbConnect";
-// import dotenv from "dotenv";
-// import router from "./routes/index";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const PORT = parseInt(process.env.PORT ?? "5000", 10);
-// const HOST = process.env.HOST ?? "localhost";
-
-// const app = express();
-
-// app.use(cors({
-//   origin: "http://localhost:3000", // Ajusta esto al origen de tu frontend
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-// }));
-
-// app.use(express.json());
-
-// app.use("/api", router);
-
-// dbConnect();
-
-// app.listen(PORT, HOST, () => {
-//   console.log(`Server is running on http://${HOST}:${PORT}`);
-// });
-
 import express from "express";
 import dbConnect from "./db/dbConnect";
 import dotenv from "dotenv";
